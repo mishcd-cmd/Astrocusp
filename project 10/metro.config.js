4055c4c1fd06d046be6076dec1895754ca78c1ac
mishcd-cmd <mish.cd@gmail.com>
2025-09-26 19:48:39 +1000

    preview test

--- project 10/metro.config.js
+++ project 10/metro.config.js
@@ -3,12 +3,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Ensure proper web configuration
-config.resolver.platforms = ['web', 'native'];
+//config.resolver.platforms = ['web', 'native'];
 
 // Add proper web configuration
-config.web = {
-  ...config.web,
-  bundler: 'metro'
-};
+//config.web = {
+ // ...config.web,
+ // bundler: 'metro'
+//};
 
 module.exports = config;
